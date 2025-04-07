df4dde4ea2774d0a68e6d4e7c83e03f0938d0b1d
Marshmallow Siskin <134213041+FinchMarshmallow@users.noreply.github.com>
2025-04-07 18:45:43 +0400

    good login

--- Project_solution_test_task/Project_solution_test_task/Controller/LoginController.cs
+++ Project_solution_test_task/Project_solution_test_task/Controller/LoginController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,8 +10,25 @@ using System.Threading.Tasks;
 namespace Project_solution_test_task.Controller
 {
 	[ApiController]
-	[Route("api/login/[controller]")]
-	public class LoginController
+	[Route("[controller]")]
+	public class LoginController : ControllerBase
 	{
+		[HttpPost("/api/login")]
+		public IActionResult Post([FromBody] LoginData data)
+		{
+			if (!ModelState.IsValid)
+			{
+				return BadRequest("error data");
+			}
+
+			Console.WriteLine($"\n data user: {data.Email.Length}, {data.Password.Length}");
+
+			return Ok(new { message = "good data" });
+		}
+		public class LoginData
+		{
+			public string Email { get; set; } = string.Empty;
+			public string Password { get; set; } = string.Empty;
+		}
 	}
 }
